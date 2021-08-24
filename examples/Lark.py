ee3391a28f563116106d64435972c285828d04ce
luxusheng <luxusheng@lunaon.com>
2021-08-24 13:33:25 +0800

    tiktok

--- examples/Lark.py
+++ examples/Lark.py
@@ -10,13 +10,62 @@ import jenkins
 import requests
 import urllib3
 
-from examples.trending import trend
+
 
 '''
 飞书推送方法：
 读取report文件中"prometheusData.txt"，循环遍历获取需要的值。
 使用飞书机器人的接口，拼接后推送text
 '''
+import json
+import random
+import requests
+import string
+import os.path
+from Naked.toolshed.shell import muterun_js
+
+def trend():
+    referer = "https://www.tiktok.com/"
+    url = "https://m.tiktok.com/api/post/item_list/?aid=1988&count=30&secUid=MS4wLjABAAAAOUoQXeHglWcq4ca3MwlckxqAe-RIKQ1zlH9NkQkbLAT_h1_6SDc4zyPdAcVdTWZF&cursor=0"
+
+    h = requests.head(
+        url,
+        headers={
+            "x-secsdk-csrf-version": "1.2.5",
+            "x-secsdk-csrf-request": "1"
+        },
+    )
+
+    csrf_session_id = h.cookies["csrf_session_id"]
+    csrf_token = h.headers["X-Ware-Csrf-Token"].split(",")[1]
+
+    response = muterun_js(' '.join([os.path.abspath('browser.js'), "\""+url+"\""]))
+
+    if response.exitcode == 0:
+        # the command was successful, handle the standard output
+        try:
+            signature = json.loads(response.stdout)
+            print(signature)
+            request = requests.get(signature['data']['signed_url'], headers={"method": "GET",
+                                                                             "accept-encoding": "gzip, deflate",
+                                                                             "cookie": "tt_webid_v2=1234567890; csrf_session_id=" + csrf_session_id,
+                                                                             "Referer": referer,
+                                                                             "user-agent": signature['data']['navigator']['user_agent'],
+                                                                             "x-secsdk-csrf-token": csrf_token,
+                                                                             "Connection": 'close'
+                                                                             })
+
+            data = request.text
+            data = json.loads(data)
+            print(len(data["itemList"]))
+            return len(data["itemList"])
+        except Exception as e:
+            return e
+    else:
+        standard_err = response.stderr
+        exit_code = response.exitcode
+        print('Cannot run node script ' + str(exit_code) + ': ' + standard_err)
+        return standard_err
 
 
 def LarkSend():
