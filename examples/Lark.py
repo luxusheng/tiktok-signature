92c89deeb6ac613d5d09073d73e5a5a38a03e5d7
luxusheng <luxusheng@lunaon.com>
2021-08-24 14:57:05 +0800

    tiktok 优化

--- examples/Lark.py
+++ examples/Lark.py
@@ -39,33 +39,32 @@ def trend():
     csrf_session_id = h.cookies["csrf_session_id"]
     csrf_token = h.headers["X-Ware-Csrf-Token"].split(",")[1]
 
-    response = muterun_js(' '.join([os.path.abspath('browser.js'), "\""+url+"\""]))
+    # response = muterun_js(' '.join([os.path.abspath('browser.js'), "\""+url+"\""]))
+    a = 'node /Users/liuwenhui/tiktok-signature/examples/browser.js "https://m.tiktok.com/api/post/item_list/?aid=1988&count=30&secUid=MS4wLjABAAAAOUoQXeHglWcq4ca3MwlckxqAe-RIKQ1zlH9NkQkbLAT_h1_6SDc4zyPdAcVdTWZF&cursor=0"'
+    response = os.popen(a)
+    text = response.read()
+    text = json.loads(text)
 
-    if response.exitcode == 0:
-        # the command was successful, handle the standard output
-        try:
-            signature = json.loads(response.stdout)
-            print(signature)
-            request = requests.get(signature['data']['signed_url'], headers={"method": "GET",
-                                                                             "accept-encoding": "gzip, deflate",
-                                                                             "cookie": "tt_webid_v2=1234567890; csrf_session_id=" + csrf_session_id,
-                                                                             "Referer": referer,
-                                                                             "user-agent": signature['data']['navigator']['user_agent'],
-                                                                             "x-secsdk-csrf-token": csrf_token,
-                                                                             "Connection": 'close'
-                                                                             })
+    # the command was successful, handle the standard output
+    try:
+        # signature = json.loads(response.stdout)
+        # print(signature)
+        request = requests.get(text['data']['signed_url'], headers={"method": "GET",
+                                                                         "accept-encoding": "gzip, deflate",
+                                                                         "cookie": "tt_webid_v2=1234567890; csrf_session_id=" + csrf_session_id,
+                                                                         "Referer": referer,
+                                                                         "user-agent": text['data']['navigator']['user_agent'],
+                                                                         "x-secsdk-csrf-token": csrf_token,
+                                                                         "Connection": 'close'
+                                                                         })
 
-            data = request.text
-            data = json.loads(data)
-            print(len(data["itemList"]))
-            return len(data["itemList"])
-        except Exception as e:
-            return e
-    else:
-        standard_err = response.stderr
-        exit_code = response.exitcode
-        print('Cannot run node script ' + str(exit_code) + ': ' + standard_err)
-        return standard_err
+        data = request.text
+        data = json.loads(data)
+        print(len(data["itemList"]))
+        return len(data["itemList"])
+    except Exception as e:
+        return e
+
 
 
 def LarkSend():
